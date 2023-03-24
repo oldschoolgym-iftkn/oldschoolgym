a0eb8341d0a127335a919d5f6c5fe0ea21a1e3ea
cracker051 <stalker1guppi@ukr.net>
2023-03-24 23:27:40 +0200

    add access check

--- osg_dj/oldschoolgym/chat/views.py
+++ osg_dj/oldschoolgym/chat/views.py
@@ -7,6 +7,7 @@ from rest_framework import status
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.decorators import permission_classes
 from .utils import get_query_params
+from user.utils import get_header_params
 from user.permissions import VerifiedOnly
 
 
@@ -31,9 +32,10 @@ class ChatAPIView(APIView):
 
 
 class MessageAPIView(APIView):
-    @swagger_auto_schema(manual_parameters=get_query_params('chat_id', 'Return messages history in chat'),
-                         responses={200: MessageSerializer(many=True)})
-    @permission_classes([VerifiedOnly])
+    permission_classes = [VerifiedOnly]
+
+    @swagger_auto_schema(manual_parameters=get_query_params('chat_id', 'Return messages history in chat')+get_header_params(),
+                         responses={200: MessageSerializer(many=True)})  # CODE SMELL?
     def get(self, request, format=None):
         try:
             chat_id = int(request.query_params.get('chat_id'))
@@ -41,18 +43,24 @@ class MessageAPIView(APIView):
             return Response('Check the chat id!', status=status.HTTP_400_BAD_REQUEST)
         # TODO: check if user has access to get history of this chat
         if Chat.objects.filter(pk=chat_id).exists():
-            msg = Chat.objects.get(pk=chat_id).messages.all()
-            serialized_msg = MessageSerializer(msg, many=True)
-            return Response(serialized_msg.data, status=status.HTTP_200_OK)
+            print(request.user.id)
+            if request.user in Chat.objects.get(pk=chat_id).users.all():
+                msg = Chat.objects.get(pk=chat_id).messages.all()
+                serialized_msg = MessageSerializer(msg, many=True)
+                return Response(serialized_msg.data, status=status.HTTP_200_OK)
+            else:
+                return Response('You dont have access to get this!', status=status.HTTP_403_FORBIDDEN)
         else:
             return Response('Chat with id could not be found!', status=status.HTTP_400_BAD_REQUEST)
 
-    @swagger_auto_schema(operation_description="Create message", request_body=MessageSerializer, responses={200: MessageSerializer})
-    @permission_classes([VerifiedOnly])
+    @swagger_auto_schema(operation_description="Create message", request_body=MessageSerializer, responses={200: MessageSerializer},
+                         manual_parameters=get_header_params())
     def post(self, request, format=None):
         msg = MessageSerializer(data=request.data)
-        # TODO: CHeck if user has access to this chat
         if msg.is_valid():
-            msg.save()
-            return Response(msg.data, status=status.HTTP_200_OK)
+            if request.user in msg.validated_data.get('chat').users.all():
+                msg.save(sender=request.user)
+                return Response(msg.data, status=status.HTTP_200_OK)
+            else:
+                return Response({'detail': ['You dont have permission to this chat']}, status=status.HTTP_403_FORBIDDEN)
         return Response(msg.errors, status=status.HTTP_400_BAD_REQUEST)
