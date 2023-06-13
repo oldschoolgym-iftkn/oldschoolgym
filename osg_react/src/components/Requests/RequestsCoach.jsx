ea0ee994c10ead29082ded31d7a14a9ffd5d5b8a
kanseidorifto <39811215+kanseidorifto@users.noreply.github.com>
2023-06-13 14:43:04 +0300

    empty requests fix

--- osg_react/src/components/Requests/RequestsCoach.jsx
+++ osg_react/src/components/Requests/RequestsCoach.jsx
@@ -58,71 +58,81 @@ const RequestsCoach = () => {
 			) : (
 				<div className="flex-1 p-8 overflow-y-auto text-lg sm:text-xl max-md:p-4">
 					<div className="flex flex-col h-full overflow-y-auto border-2 border-black rounded-3xl">
-						<div className="flex items-center justify-between px-2 py-1 sm:px-3">
-							<div className="flex items-center space-x-4">
-								<button onClick={() => setSelection(myApplications.data?.map((app) => app.id))}>
-									<CheckCircleIcon className="w-6 h-6 sm:w-8 sm:h-8" />
-								</button>
-								<button>
-									<EllipsisVerticalIcon className="w-6 h-6 sm:w-8 sm:h-8" />
-								</button>
-							</div>
-							<div className="flex items-center space-x-2">
-								<span>
-									{1}/{1}
-								</span>
-								<button>
-									<ArrowLeftIcon className="w-6 h-6 sm:w-8 sm:h-8" />
-								</button>
-								<button>
-									<ArrowRightIcon className="w-6 h-6 sm:w-8 sm:h-8" />
-								</button>
-							</div>
-						</div>
-						<div className="flex flex-1">
-							<div className="my-3 mx-2 sm:m-3 space-y-[25px]">
-								{myApplications.data?.map((obj, index) => (
-									<button
-										onClick={() =>
-											selection.find((id) => id === obj.id)
-												? setSelection((prev) => prev.filter((id) => id !== obj.id))
-												: setSelection((prev) => [...prev, obj.id])
-										}
-										key={obj.id}
-										className="block">
-										{selection.find((id) => id === obj.id) ? (
+						{myApplications.data?.length > 0 ? (
+							<>
+								<div className="flex items-center justify-between px-2 py-1 sm:px-3">
+									<div className="flex items-center space-x-4">
+										<button onClick={() => setSelection(myApplications.data?.map((app) => app.id))}>
 											<CheckCircleIcon className="w-6 h-6 sm:w-8 sm:h-8" />
-										) : (
-											<StopIcon className="w-6 h-6 sm:w-8 sm:h-8" />
-										)}
-									</button>
-								))}
-							</div>
-							<div className="flex-1 border-t border-l border-black">
-								{myApplications.data?.map((application, index) => (
-									<div key={index} className="border-b border-black">
-										<div className="flex items-center transition ease-out duration-200 bg-transparent rounded-3xl hover:bg-neutral-400/90 hover:scale-[101%] hover:rounded-3xl">
-											<button
-												onClick={() => {
-													navigate('/cabinet/requests/' + application.id);
-												}}
-												className="flex items-center flex-1 flex-grow py-2 text-left justify-items-center">
-												<img
-													src={process.env.REACT_APP_API_URL + application.user.avatar}
-													alt="Img"
-													className="inline-block w-8 h-8 mx-2 border border-black rounded-full sm:w-10 sm:h-10 sm:mx-3"
-												/>
-												<div className="flex flex-1 space-x-2 truncate">
-													<p className="font-semibold">{application.user.first_name}</p>
-													<p className="text-neutral-700">{application.subject}</p>
-												</div>
-												<p className="px-2 text-base sm:text-lg">{new Date().getDate()} чер.</p>
-											</button>
-										</div>
+										</button>
+										<button>
+											<EllipsisVerticalIcon className="w-6 h-6 sm:w-8 sm:h-8" />
+										</button>
 									</div>
-								))}
+									<div className="flex items-center space-x-2">
+										<span>
+											{1}/{1}
+										</span>
+										<button>
+											<ArrowLeftIcon className="w-6 h-6 sm:w-8 sm:h-8" />
+										</button>
+										<button>
+											<ArrowRightIcon className="w-6 h-6 sm:w-8 sm:h-8" />
+										</button>
+									</div>
+								</div>
+								<div className="flex flex-1">
+									<div className="my-3 mx-2 sm:m-3 space-y-[25px]">
+										{myApplications.data?.map((obj, index) => (
+											<button
+												onClick={() =>
+													selection.find((id) => id === obj.id)
+														? setSelection((prev) => prev.filter((id) => id !== obj.id))
+														: setSelection((prev) => [...prev, obj.id])
+												}
+												key={obj.id}
+												className="block">
+												{selection.find((id) => id === obj.id) ? (
+													<CheckCircleIcon className="w-6 h-6 sm:w-8 sm:h-8" />
+												) : (
+													<StopIcon className="w-6 h-6 sm:w-8 sm:h-8" />
+												)}
+											</button>
+										))}
+									</div>
+									<div className="flex-1 border-t border-l border-black">
+										{myApplications.data?.map((application, index) => (
+											<div key={index} className="border-b border-black">
+												<div className="flex items-center transition ease-out duration-200 bg-transparent rounded-3xl hover:bg-neutral-400/90 hover:scale-[101%] hover:rounded-3xl">
+													<button
+														onClick={() => {
+															navigate('/cabinet/requests/' + application.id);
+														}}
+														className="flex items-center flex-1 flex-grow py-2 text-left justify-items-center">
+														<img
+															src={process.env.REACT_APP_API_URL + application.user.avatar}
+															alt="Img"
+															className="inline-block w-8 h-8 mx-2 border border-black rounded-full sm:w-10 sm:h-10 sm:mx-3"
+														/>
+														<div className="flex flex-1 space-x-2 truncate">
+															<p className="font-semibold">{application.user.first_name}</p>
+															<p className="text-neutral-700">{application.subject}</p>
+														</div>
+														<p className="px-2 text-base sm:text-lg">{new Date().getDate()} чер.</p>
+													</button>
+												</div>
+											</div>
+										))}
+									</div>
+								</div>
+							</>
+						) : (
+							<div className="py-6 mx-auto">
+								<p className="text-lg text-center sm:text-2xl">
+									У вас ще немає відправлених заявок.😥
+								</p>
 							</div>
-						</div>
+						)}
 					</div>
 				</div>
 			)}
